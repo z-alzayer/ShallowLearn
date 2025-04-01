60ff8315c8a9ea707816a5b5582dcf0a777448a8
z-alzayer <zba21@ic.ac.uk>
2025-04-01 15:43:14 +0100

    move extract_point to Util which is more logical

--- ShallowLearn/Util.py
+++ ShallowLearn/Util.py
@@ -258,3 +258,22 @@ def clip_image(image, clip_percent=1):
     clipped_image = (clipped_image - lower_clip) / ((upper_clip - lower_clip) + 0.001)
     
     return clipped_image
+
+def extract_point_spectra(array, x, y):
+    """
+    Extracts the spectra of a single point (x, y) across all time slices.
+    
+    Parameters:
+    - array: numpy array of shape (t, x, y, z)
+    - x: x-coordinate of the point
+    - y: y-coordinate of the point
+    
+    Returns:
+    - spectra: numpy array of shape (t, z)
+    """
+    
+    if len(array.shape) != 4:
+        raise ValueError("Input array should be of shape (t, x, y, z)")
+    
+    spectra = array[:, x, y, :]
+    return spectra
